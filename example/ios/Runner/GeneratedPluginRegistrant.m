cfe4c1f2651145d7976b8410677c88879b3a6892
mingV <smw059184@163.com>
2022-07-28 17:01:42 +0800

    moblink-flutter-no-null-safety

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<moblink/MoblinkPlugin.h>)
+#import <moblink/MoblinkPlugin.h>
+#else
+@import moblink;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [MoblinkPlugin registerWithRegistrar:[registry registrarForPlugin:@"MoblinkPlugin"]];
+}
+
+@end
